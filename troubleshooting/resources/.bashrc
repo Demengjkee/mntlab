c320b87d62e80eb8288508f3606c21c65d4e393d
Demengjkee <ncherepkin@live.com>
2016-07-13 12:29:06 +0300

    tomcat now works on 127.0.0.1 and tomcat init script now check if tomcat was actually started

---
+++ troubleshooting/resources/.bashrc
@@ -0,0 +1,9 @@
+# .bashrc
+
+# Source global definitions
+if [ -f /etc/bashrc ]; then
+	. /etc/bashrc
+fi
+
+# User specific aliases and functions
+export JAVA_HOME=/opt/oracle/java/x64/jdk1.7.0_79
--- troubleshooting/resources/fix.sh
+++ troubleshooting/resources/fix.sh
@@ -5,14 +5,14 @@ chattr -i /etc/sysconfig/iptables
 iptables-restore < /vagrant/resources/iptables
 iptables-save > /etc/sysconfig/iptables
 
-echo "JAVA_HOME=/opt/oracle/java/x64/jdk1.7.0_79" > /opt/apache/tomcat/current/bin/setenv.sh
-chown -R tomcat:tomcat /opt/apache/tomcat/
-cp -f /vagrant/resources/tomcat /etc/init.d/
-echo > /home/tomcat/.bashrc
-chkconfig tomcat on
-/etc/init.d/tomcat start
-
 cp -f /vagrant/resources/workers.properties /etc/httpd/conf.d/
 cp -f /vagrant/resources/vhost.conf /etc/httpd/conf.d/
 cp -f /vagrant/resources/httpd.conf /etc/httpd/conf/
 apachectl restart
+
+chown tomcat:tomcat /opt/apache/tomcat/current/logs/
+cp -f /vagrant/resources/tomcat /etc/init.d/
+cp -f /vagrant/resources/.bashrc /home/tomcat/
+cp -f /vagrant/resources/server.xml /opt/apache/tomcat/current/conf/
+chkconfig tomcat on
+/etc/init.d/tomcat start
